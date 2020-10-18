6c172f3a4f9fdfd4cddd3911a22e8a3d5660e941
Marcus Chiu <marcuschiu9@gmail.com>
2020-10-18 13:26:05 -0500

    initial commit

---
+++ src/main/java/com/marcuschiu/aspectorientedprogramming/AspectOrientedProgrammingApplication.java
@@ -0,0 +1,13 @@
+package com.marcuschiu.aspectorientedprogramming;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AspectOrientedProgrammingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AspectOrientedProgrammingApplication.class, args);
+	}
+
+}
